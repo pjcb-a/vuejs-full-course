9b3d73a1d336432f3cfd7ccb46cd764b24f06245
John Paul Jacob <jacob.johnpaul15@gmail.com>
2026-03-27 17:16:34 +0800

    created Dashboard and Workout views

---
+++ src/components/pages/Dashboard.vue
@@ -0,0 +1,45 @@
+ <script setup>
+    import Grid from '../Grid.vue'
+    import { gymHealthFacts } from '../../utils';
+    const randomNumber = Math.floor(Math.random() * gymHealthFacts.length);
+    const todaysFact = gymHealthFacts[randomNumber];
+</script>
+
+<template>
+    <section id="dashboard">
+        <div class="card tip-container">
+            <h2>Welcome Player</h2>
+            <div>
+                <p class="tip"><strong>Daily Tip</strong><br/> {{ todaysFact }}</p>
+            </div>
+            <button>Start Workout &rarr;</button>
+        </div>
+        <Grid />
+    </section>
+</template>
+
+<style scoped>
+    .tip-container, .tip-container div, 
+    #dashboard {
+        display: flex;
+    }
+
+    .tip-container, 
+    #dashboard {
+        flex-direction: column;
+    }
+
+    #dashboard {
+        gap: 2rem;
+    }
+
+    .tip-container {
+        gap: 0.5rem;
+    }
+
+    @media (min-width: 640px) {
+        .tip-container {
+            gap: 1rem;
+        }
+    }
+</style>
